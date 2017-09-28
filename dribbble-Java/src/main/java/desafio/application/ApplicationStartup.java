77d5643993fb0ab4ec037d634e1ae617c54dfe5d
freitass <sfreitas@fitec.org.br>
2017-09-28 17:57:10 -0300

    Initial setup

--- dribbble-Java/src/main/java/desafio/application/ApplicationStartup.java
+++ dribbble-Java/src/main/java/desafio/application/ApplicationStartup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableAutoConfiguration
-public class ApplicationStartup 
+public class ApplicationStartup
 {
-	public static void main(String[] args)
-	{
+    public static void main(String[] args)
+    {
         SpringApplication.run(ApplicationStartup.class, args);
     }
 }
