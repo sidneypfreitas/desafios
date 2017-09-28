8c6fd44e6545d22a9ac449a312235156edab84b0
freitass <sfreitas@fitec.org.br>
2017-09-28 17:36:38 -0300

    Initial setup

---
+++ dribbble-Java/src/main/java/desafio/application/ApplicationStartup.java
@@ -0,0 +1,13 @@
+package desafio.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class ApplicationStartup 
+{
+	public static void main(String[] args)
+	{
+        SpringApplication.run(ApplicationStartup.class, args);
+    }
+}
